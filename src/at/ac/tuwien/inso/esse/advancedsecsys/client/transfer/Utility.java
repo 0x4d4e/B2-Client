e301f75c7445981a490f77fec3e28af982e58d91
Fabian Souczek <e0728541@student.tuwien.ac.at>
2013-01-14 11:30:53 +0100

    corrected line ending in query

--- src/at/ac/tuwien/inso/esse/advancedsecsys/client/transfer/Utility.java
+++ src/at/ac/tuwien/inso/esse/advancedsecsys/client/transfer/Utility.java
@@ -2,21 +2,19 @@ package at.ac.tuwien.inso.esse.advancedsecsys.client.transfer;
 
 public class Utility
 {
-  private static String lf = System.getProperty("line.separator");
-
   public static String b64d(String paramString)
   {
     String str = new String(Base64.decode(paramString.getBytes(), 0));
-    if (str.endsWith(lf))
-      str = str.substring(0, str.length() - lf.length());
+    if (str.endsWith("\n"))
+      str = str.substring(0, str.length() - 1);
     return str;
   }
 
   public static String b64e(String paramString)
   {
     String str = new String(Base64.encode(paramString.getBytes(), 0));
-    if (str.endsWith(lf))
-      str = str.substring(0, str.length() - lf.length());
+    if (str.endsWith("\n"))
+        str = str.substring(0, str.length() - 1);
     return str;
   }
 }
